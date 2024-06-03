700ae6cc4911301950b33668ecc1d2866f03f0ab
fdattel <jindrich.zdansky@gmail.com>
2024-06-03 11:06:54 +0200

    fix download urls

--- scripts/ntx20-get.bat
+++ scripts/ntx20-get.bat
@@ -1,1 +1,1 @@
- curl https://s3.eu-central-1.wasabisys.com/apps.nanotrix.cloud/install/ntx20.tgz --output - | tar -xzvf - -C %~dp0
+ curl https://s3.eu-central-1.wasabisys.com/apps.ntx20.cloud/install/ntx20.tgz --output - | tar -xzvf - -C %~dp0
