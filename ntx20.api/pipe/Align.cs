0c8d57dfc7c0f745495a94a87055f7827d758347
fdattel <jindrich.zdansky@gmail.com>
2025-03-25 22:31:44 +0100

    eval impl

---
+++ ntx20.api/pipe/Align.cs
@@ -0,0 +1,215 @@
+﻿using ntx.api.utils;
+using ntx20.api.proto;
+using ntx20.api.proto.legacy.v2t.engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ntx20.api.pipe
+{
+    public static partial class Pipe
+    {
+        
+        internal static async IAsyncEnumerable<proto.Payload> ViaReLabelAndSplit(this IAsyncEnumerable<proto.Payload> source, Regex split, Regex plus)
+        {
+
+            await foreach (var item in source)
+            {
+                var ret = new proto.Payload { Track = item.Track };
+                foreach (var i in item.Chunk)
+                {
+                    if (i.Key != "txt")
+                    {
+                        ret.Chunk.Add(i);
+                        continue;
+                    }
+
+                    
+                    foreach (var s in split.Split(i.S))
+                    {
+                        if (s == "")
+                            continue;
+                        if (plus.IsMatch(s))
+                        {
+                            ret.Chunk.Add(
+                                new proto.Item { Key = "txt", S = s, Tags = { "+"} }
+                                );
+                        }
+                        else
+                        {
+                            ret.Chunk.Add(
+                                new proto.Item { Key = "txt", S = s }
+                                );
+                        }
+                    }
+                }
+                yield return ret;
+            }
+        }
+        internal static async IAsyncEnumerable<proto.Payload> ViaLabelMerge(this IAsyncEnumerable<proto.Payload> source)
+        {
+            
+            await foreach (var item in source)
+            {
+                var ret = new proto.Payload { Track = item.Track };
+                var buff = new List<proto.Item>();
+                var pos = item.Chunk.GetEnumerator();
+                bool? isPlus = null;
+                foreach (var i in item.Chunk)
+                {
+                    if (i.Key != "txt")
+                    {
+                        if (isPlus == null)
+                        {
+                            ret.Chunk.Add(i);
+                        }
+                        else
+                        {
+                            buff.Add(i);
+                        }
+                        continue;
+                    }
+                    if(isPlus == null)
+                    {
+                        isPlus = i.Tags.Contains("+");
+                        buff.Add(i);
+                        continue;
+                    }
+                    if (isPlus != i.Tags.Contains("+"))
+                    {
+                        ret.Chunk.AddRange(buff);
+                        buff.Clear();
+                        buff.Add(i);
+                        isPlus=i.Tags.Contains("+");
+                        continue;
+                    }
+                    /*
+                    if (isPlus == true)
+                    {
+                        ret.Chunk.AddRange(buff);
+                        buff.Clear();
+                        buff.Add(i);
+                    }
+                    else
+                    */
+                    {
+                        i.S = buff[0].S + i.S;
+                        buff.Clear();
+                        buff.Add(i);
+                    }
+                    
+                }
+                ret.Chunk.AddRange(buff);
+                yield return ret;
+            }
+        }
+
+        internal static IEnumerable<TextItem> ToTextItemRef(this IEnumerable<proto.Payload> source)
+        {
+            var toNotEval = new string[] { "noise", "+" };
+            int block = 0;
+            int idg = 0;
+            var cSpeaker = "nobody";
+            foreach (var item in source)
+            {
+                int id = 0;
+                foreach ( var i in item.Chunk)
+                {
+                    if (i.Key == "spk")
+                    {
+                        cSpeaker = i.S;
+                    }
+                        if (i.Key == "txt")
+                    {
+                        bool eval = i.Tags.Intersect(toNotEval).Count() == 0;
+                        if (i.Tags.Contains("+"))
+                        {
+                            yield return new TextItem { Block = block,Value= i.S ,Index = idg,
+                                Text = "#!ref" +  idg.ToString("000000000") +  i.S, Speaker=cSpeaker,
+                                Eval = eval
+                            };
+                        }
+                        else
+                        {
+                            yield return new TextItem { Block = block,Value =i.S ,Index = idg, Text = i.S, Speaker=cSpeaker, Eval = eval };
+                        }
+                    }
+                    id++;
+                    idg++;
+                }
+                block++;
+            }
+        }
+        internal static IEnumerable<TextItem> ToTextItemRes(this IEnumerable<proto.Payload> source)
+        {
+            var toNotEval = new string[] { "noise", "+" };
+            int block = 0;
+            int idg = 0;
+            var cSpeaker = "nobody";
+            TextItem cItem = null;
+            var lastTs = -1.0;
+            foreach (var item in source)
+            {
+                int id = 0;
+                foreach (var i in item.Chunk)
+                {
+                    var lastText = "";
+                    if (i.Key == "ts")
+                    {
+                        lastTs = i.D;
+                        
+                    }
+                    if (i.Key == "spk")
+                    {
+                        cSpeaker = i.S;
+                    }
+                    if (i.Key == "txt")
+                    {
+                        bool eval = i.Tags.Intersect(toNotEval).Count() == 0;
+                        if (i.Tags.Contains("+"))
+                        {
+                            cItem = new TextItem { Block = block, Value = i.S, Index = idg, 
+                                Text = "#!res" + idg.ToString("000000000") + i.S , Speaker=cSpeaker, Eval =eval, Start=lastTs};
+                        }
+                        else
+                        {
+                            cItem =  new TextItem { Block = block, Value = i.S, Index = idg, Text = i.S , Speaker = cSpeaker, Eval = eval, Start=lastTs };
+                        }
+                    }
+                    id++;
+                    idg++;
+                }
+                block++;
+            }
+        }
+        public static List<AlignBlock> ToAlignBlocks(this List<List<AlignedItem>> alignBlocks, Item[] refList, Item[] resList)
+        {
+            
+            return null;
+        }
+
+
+
+        public static async IAsyncEnumerable<proto.Payload> AlignWith(this IAsyncEnumerable<proto.Payload> source, 
+            IAsyncEnumerable<proto.Payload> reference, string SplitOption, string PlusOption)
+        {
+            Regex split = new Regex(SplitOption);
+            Regex plus = new Regex(PlusOption);
+
+            //Expecting ordered in blocks
+            var rfr = await reference.ViaReLabelAndSplit(split, plus).ViaLabelMerge().ToArrayAsync();
+            var src = await source.ViaReLabelAndSplit(split, plus).ViaLabelMerge().ToArrayAsync();
+
+            var aligment = rfr.ToTextItemRef().ToList().AlignWith(src.ToTextItemRes().ToList(), true, 1000);
+                           //.ToAlignBlocks(rfr.SelectMany(x=>x.Chunk).ToArray(), rfr.SelectMany(x=>x.Chunk).ToArray());
+
+            
+
+            yield return new proto.Payload();
+
+        }
+    }
+}
---
+++ ntx20/command/util/tran/align/Command.cs
@@ -0,0 +1,148 @@
+﻿using System.Threading;
+using Microsoft.Extensions.CommandLineUtils;
+using System.Threading.Tasks;
+using ntx20.api.io;
+using ntx20.api;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using ntx20.api.pipe;
+using ntx20.api.utils;
+using Google.Protobuf.Collections;
+using Microsoft.Extensions.Logging;
+using System;
+using Google.Protobuf.WellKnownTypes;
+using System.Data;
+using System.IO.Pipelines;
+
+namespace ntx20.command.util.tran.align
+{
+    class Command : ICommand
+    {
+        private static readonly ILogger _logger = Logging.LoggerFactory.CreateLogger("ntx20.command.util.tran.eval.one");
+
+        internal static void Configure(CommandLineApplication command, CommandLineOptions options)
+        {
+
+            command.Description = "eval one";
+            command.HelpOption("-h|--help");
+            command.ExtendedHelpText =
+             Environment.NewLine
+             + $"default split: {Constants.DefaultTextSplit}" + Environment.NewLine
+             + $"default match: {Constants.DefaultPlusMatch}" + Environment.NewLine;
+
+            var outputUriOption = command.Option("-o|--output <->",
+                "output proto url",
+                CommandOptionType.SingleValue
+                );
+            var inputUriOption = command.Option(@"-i|--input",
+            "json url",
+            CommandOptionType.SingleValue
+            );
+
+            var iFormat = command.Option($"--iformat <json>",
+              "read input as json|txt|trsx",
+              CommandOptionType.SingleValue
+              );
+
+            var rFormat = command.Option($"--rformat <json>",
+              "read input as json|txt|trsx",
+              CommandOptionType.SingleValue
+              );
+
+
+            var splitOption = command.Option($"-s|--split <default>",
+                "split regex",
+                CommandOptionType.SingleValue
+                );
+
+            var plusOption = command.Option($"-p|--plus <default>",
+                "match plus items",
+                CommandOptionType.SingleValue
+                );
+
+            var refUriOption = command.Option(@"-r|--reference",
+            "reference url",
+            CommandOptionType.SingleValue
+            );
+
+            var flush = command.Option("-f|--flush",
+                "enable flush on every write",
+                CommandOptionType.NoValue
+                );
+            var oFormat = command.Option($"-w|--writer <json>",
+                "write output as json|html",
+                CommandOptionType.SingleValue
+                );
+
+
+            command.OnExecute(() =>
+            {
+                inputUriOption.MustSetValue(command);
+                options.Command = new Command(command)
+                {
+                    OutputUriOption = outputUriOption.GetValueOrDefault(),
+                    InputUriOption = inputUriOption.Value(),
+                    RefUriOption = refUriOption.Value(),
+                    OFormat = oFormat.GetValueOrDefault(),
+                    IFormat = iFormat.GetValueOrDefault(),
+                    RFormat = rFormat.GetValueOrDefault(),
+                    Flush = flush.HasValue(),
+                    SplitOption = splitOption.GetValueOrDefault()=="default" ? Constants.DefaultTextSplit : splitOption.GetValueOrDefault(),
+                    PlusOption = plusOption.GetValueOrDefault() == "default" ? Constants.DefaultPlusMatch : plusOption.GetValueOrDefault(),
+                };
+                return 0;
+            });
+        }
+
+        private string OutputUriOption { get; set; }
+        private string InputUriOption { get; set; }
+        private string RefUriOption { get; set; }
+        private string TrackOption { get; set; }
+        private string OFormat { get; set; }
+        private string IFormat { get; set; }
+        private string RFormat { get; set; }
+        private string SplitOption { get; set; }
+        private string PlusOption { get; set; }
+        private bool Flush { get; set; }
+        private readonly CommandLineApplication _app;
+
+        public Command(CommandLineApplication app)
+        {
+            _app = app;
+        }
+        public async Task<int> RunAsync(CancellationToken breaker)
+        {
+
+            using var output = LazyStream.Output(OutputUriOption, "application/json");
+            using var input = LazyStream.Input(InputUriOption, breaker);
+            using var reference = LazyStream.Input(RefUriOption, breaker);
+
+
+            var pipe = (IFormat switch
+            {
+                "json" => input.AsProtoJsonSource<api.proto.Payload>(breaker),
+                "trsx" => input.AsTrsxTranSource(breaker),
+                _ => throw new NotImplementedException($"unsuported input format {IFormat}"),
+            });
+
+            var rpipe = (RFormat switch
+            {
+                "json" => reference.AsProtoJsonSource<api.proto.Payload>(breaker),
+                "trsx" => reference.AsTrsxTranSource(breaker),
+                _ => throw new NotImplementedException($"unsuported reference format {IFormat}"),
+            });
+
+            pipe = pipe.AlignWith(rpipe, SplitOption, PlusOption);
+
+            await (OFormat switch
+            {
+                "json" => pipe.RunWithSink(output.AsJsonProtoSink<api.proto.Payload>(), autoFlush: Flush, cancellationToken: breaker),
+                _ => throw new NotImplementedException($"unsuported output format {OFormat}"),
+            });
+            output.Complete();
+            return 0;
+        }
+    }
+}
+
